7848a872a8ff11b0272da0aa49df2b4e54c1e6d9
Ashwin Hari <ashari@microsoft.com>
2022-09-07 10:13:13 -0700

    9/7 add more BFP types and update docs.

--- onnxruntime/core/graph/contrib_ops/quantization_defs.h
+++ onnxruntime/core/graph/contrib_ops/quantization_defs.h
@@ -21,11 +21,41 @@ void ValidateTypeAndShapeForScaleAndZP(ONNX_NAMESPACE::InferenceContext& ctx, in
 
 // Names follow the convention of BFP_<# sign bits>_<# mantissa bits>_<# exponent bits>_<size of bounding box>
 enum class BFPType : int64_t {
-  // 1 sign bit, 8 mantissa bits, 8 exponent bits, 16 numbers per bounding box
+  // 1 sign bit, 8 mantissa bits, 8 exponent bits, 16 numbers per bounding box.
   BFP_1_8_8_16,
 
-  // 1 sign bit, 8 mantissa bits, 8 exponent bits, 16 numbers per bounding box
+  // 1 sign bit, 8 mantissa bits, 8 exponent bits, 16 numbers per bounding box.
   BFP_1_4_8_16,
+
+  // No sign bit, 8 mantissa bits, 8 exponent bits, 64 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_8_8_64,
+
+  // No sign bit, 8 mantissa bits, 8 exponent bits, 128 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_8_8_128,
+
+  // No sign bit, 8 mantissa bits, 8 exponent bits, 256 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_8_8_256,
+
+  // No sign bit, 4 mantissa bits, 8 exponent bits, 128 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_4_8_128,
+
+  // No sign bit, 4 mantissa bits, 8 exponent bits, 256 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_4_8_256,
+
+  // No sign bit, 16 mantissa bits, 8 exponent bits, 64 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_16_8_64,
+
+  // No sign bit, 16 mantissa bits, 8 exponent bits, 128 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_16_8_128,
+
+  // No sign bit, 16 mantissa bits, 8 exponent bits, 256 numbers per bounding box. Mantissa and exponent are signed.
+  BFP_0_16_8_256,
+
+  // No sign bit, 16 mantissa bits, 8 exponent bits, 1 number per bounding box. Mantissa and exponent are signed.
+  BFP_0_16_8_1,
+
+  // No sign bit, 24 mantissa bits, 8 exponent bits, 1 number per bounding box. Mantissa and exponent are signed.
+  BFP_0_24_8_1,
 
   // Reserved for custom BFP types
   Custom_BFP_0,
