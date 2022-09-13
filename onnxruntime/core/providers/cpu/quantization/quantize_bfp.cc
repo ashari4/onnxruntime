4ad8f1766874f4ad8b4bca70b35eaabf3d8296c1
Ashwin Hari <ashari@microsoft.com>
2022-09-13 12:57:56 -0700

    standard op works

--- onnxruntime/core/providers/cpu/quantization/quantize_bfp.cc
+++ onnxruntime/core/providers/cpu/quantization/quantize_bfp.cc
@@ -7,19 +7,45 @@
 #include "core/util/qmath.h"
 
 // todo: ah remove this
+#include <algorithm>
 #include <iostream>
 
 namespace onnxruntime {
 
-Status DequantizeBFP::Compute(OpKernelContext* /* ctx */) const {
+Status DequantizeBFP::Compute(OpKernelContext* ctx) const {
   std::cout << "in DequantizeBFP::Compute" << std::endl;
-  // todo: ah create fp32 output tensor
+  auto& shape = *ctx->Input<Tensor>(1);
+  // todo: ah use the stride
+  [[maybe_unused]] auto& stride = *ctx->Input<Tensor>(2);
+  TensorShape output_shape(shape.Data<int64_t>(), shape.Shape().Size());
+  auto& output = ctx->RequiredOutput(0, output_shape);
+
+  auto& raw_bfp = *ctx->Input<Tensor>(0);
+  auto begin = raw_bfp.Data<uint8_t>();
+  auto end = begin + raw_bfp.SizeInBytes();
+  std::copy(begin, end, reinterpret_cast<uint8_t *>(output.MutableDataRaw()));
   return Status::OK();
 }
 
-Status QuantizeBFP::Compute(OpKernelContext* /* ctx */) const {
+Status QuantizeBFP::Compute(OpKernelContext* ctx) const {
   std::cout << "in QuantizeBFP::Compute" << std::endl;
-  // todo: ah create uint8 tensor, shape and stride
+  const auto& input = *ctx->Input<Tensor>(0);
+  auto size = input.SizeInBytes();
+  // for demo purposes, bfp tesor is just a copy of the original tensor
+  auto& raw_bfp = ctx->RequiredOutput(0, {static_cast<int64_t>(size)});
+  auto begin = reinterpret_cast<const uint8_t*>(input.DataRaw());
+  std::copy(begin, begin + size, raw_bfp.MutableData<uint8_t>());
+
+  auto& input_shape = input.Shape();
+  auto& shape = ctx->RequiredOutput(1, {static_cast<int64_t>(input_shape.NumDimensions())});
+  input.Shape().CopyDims(shape.MutableData<int64_t>(), input_shape.NumDimensions());
+  [[maybe_unused]] auto& stride = ctx->RequiredOutput(2, {static_cast<int64_t>(input_shape.NumDimensions())});
+#ifdef ENABLE_TRAINING
+  // todo: ah copy stride data from input
+#else
+  // stride data not available - assume the output is contiguous.
+  // todo: ah set the strides to be contiguous.
+#endif
   return Status::OK();
 }
 }  // namespace onnxruntime
